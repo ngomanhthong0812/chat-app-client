018405bcf7a2afbbef9f539885091a1bfb29eede
Ngo Manh Thong <ngomanhthong0812>
2024-10-28 15:51:50 +0700

    update verson react && react-dom

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -5,11 +5,10 @@ const SidebarNavigation: React.FC<IProps> = () => {
         <div className="flex justify-center">
             <div className="navbar">
                 {SIDEBAR_NAV.map(item => (
-                    <div className={`p-3 rounded-lg text-[#b0b3b8] cursor-pointer hover:bg-[#47484b] bg-opacity-75 ${item.active && 'bg-[#47484b] !text-[white]'}`}>
+                    <div className={`navbar-item p-3 rounded-lg text-[#b0b3b8] cursor-pointer hover:bg-[#47484b] bg-opacity-75 ${item.active && 'bg-[#47484b] !text-[white]'}`}>
                         {item.icon}
                     </div>
                 ))}
-                demo
             </div>
         </div>
     );
