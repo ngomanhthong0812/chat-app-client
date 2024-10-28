c4824fd354dc768ceb53bce2b7f94f8af6860198
Ngo Manh Thong <ngomanhthong0812>
2024-10-28 15:16:43 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Link from "next/link";
+import SidebarNavigation from "../components/SidebarNavigation";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="text-5xl">
-      <Link href="/login" className="text-blue-600 bg-red-600">
-        Login
-      </Link>
-    </div>
+    <main className=" bg-[#1a1b1b] h-[100vh] p-4">
+
+      <div className="container h-full  flex  gap-4  m-auto">
+        <div className="w-[3%]">
+          <SidebarNavigation />
+        </div>
+
+        {/* 24.5% */}
+        <div className="min-w-[32.5%] bg-[#47484b] bg-opacity-15 rounded-xl shadow-custom-light">
+        </div>
+
+        <div className="flex-1 flex gap-4">
+          <div className="flex-1 bg-[#47484b] bg-opacity-15 rounded-xl shadow-custom-light">
+
+          </div>
+          {/* <div className="min-w-[33%] bg-[#47484b] bg-opacity-15 rounded-xl shadow-custom-light">
+
+        </div> */}
+        </div>
+      </div>
+
+    </main>
   );
 }
+export default Home;
---
+++ src/components/SidebarNavigation.tsx
@@ -0,0 +1,16 @@
+import { SIDEBAR_NAV } from '../constants/SidebarNavigation'
+interface IProps { }
+const SidebarNavigation: React.FC<IProps> = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="navbar">
+                {SIDEBAR_NAV.map(item => (
+                    <div className={`p-3 rounded-lg text-[#b0b3b8] cursor-pointer hover:bg-[#47484b] bg-opacity-75 ${item.active && 'bg-[#47484b] !text-[white]'}`}>
+                        {item.icon}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+export default SidebarNavigation;
