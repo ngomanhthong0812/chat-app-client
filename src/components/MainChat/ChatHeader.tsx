c4b73f7f038517f9f22df9cb97dd7e3a0d0b31ad
quangtrinhhh <trinhngocquanggg@gmail.com>
2024-10-30 13:43:55 +0700

    thêm dấu hoạt động và fix scroll messgae

--- src/components/MainChat/ChatHeader.tsx
+++ src/components/MainChat/ChatHeader.tsx
@@ -19,8 +19,9 @@ const ChatHeader: NextPage<Props> = ({ toggleChatInfo }) => {
   return (
     <div className="p-3 flex justify-between items-center text-center shadow-sm ">
       <div className="flex text-start items-center ">
-        <div className="w-9 h-9 mr-3">
+        <div className="w-9 h-9 mr-3 relative">
           <img src="/avatar-trang-4.jpg" alt="" className="rounded-full" />
+          <span className="absolute right-[1px] bottom-0 w-[14px] h-[14px] bg-green-600 rounded-full border-2 border-[#252323]"></span>
         </div>
         <div className="text-white flex flex-col">
           <span className="text-[15px] font-medium leading-[19px]">Quang</span>
@@ -35,8 +36,11 @@ const ChatHeader: NextPage<Props> = ({ toggleChatInfo }) => {
           <IoIosVideocam size={22} />
         </div>
         <div
-          className={`flex items-center justify-center rounded-full cursor-pointer ${isActive ? "bg-[#aa00ff] text-[#3e4042] text-sm w-5 h-5" : "hover:bg-[#3e4042] text-xl w-7 h-7"
-            }`} // Áp dụng class tùy thuộc vào isActive
+          className={`flex items-center justify-center rounded-full cursor-pointer ${
+            isActive
+              ? "bg-[#aa00ff] text-[#3e4042] text-sm w-5 h-5"
+              : "hover:bg-[#3e4042] text-xl w-7 h-7"
+          }`} // Áp dụng class tùy thuộc vào isActive
           onClick={handleToggle} // Gọi handleToggle
         >
           <BsThreeDots />
